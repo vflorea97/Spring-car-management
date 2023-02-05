0a830fb85c3907d447ecc3c16b021177203b0064
Valentin <vflorea97@yahoo.com>
2023-02-05 15:51:32 +0200

    create view

--- car-management/src/main/java/ro/mycode/carmanagement/CarManagementApplication.java
+++ car-management/src/main/java/ro/mycode/carmanagement/CarManagementApplication.java
@@ -1,7 +1,11 @@
 package ro.mycode.carmanagement;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import ro.mycode.carmanagement.view.View;
 
 @SpringBootApplication
 public class CarManagementApplication {
@@ -10,4 +14,18 @@ public class CarManagementApplication {
 		SpringApplication.run(CarManagementApplication.class, args);
 	}
 
+
+
+	@Bean
+	CommandLineRunner commandLineRunner(View view) {
+		return args -> {
+
+
+
+
+			view.play();
+
+		};
+
+	}
 }
